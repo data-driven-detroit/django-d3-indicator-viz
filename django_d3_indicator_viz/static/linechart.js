7e9c3f2544e9a743082e4959775e887c98c5529f
Mike V <mike@datadrivendetroit.org>
2025-12-15 15:49:51 -0500

    trying to fix the linechart gappyness

--- django_d3_indicator_viz/static/linechart.js
+++ django_d3_indicator_viz/static/linechart.js
@@ -65,7 +65,7 @@ export default class LineChart {
 
         // set up the container
         this.container.classList.add('line-chart-container');
-        this.container.style.height = '300px';
+        this.container.style.height = '240px';
 
         // dispose the old chart (if redrawing)
         if (this.chart) {
